592f5ada76f259c39a6818144efdcea0864b8ec1
Mathew Fischbach <fisch872@umn.edu>
2020-09-16 11:05:52 -0500

    working version of imputation pipeline with Impute5 and SHapeit4 implemented. Edited genetic map files and added requirements.txt

--- Impute5Pipeline.py
+++ Impute5Pipeline.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.8
+#!/project/csbio/MathewF/bin/python3.8 -u
 # # Copyright Mathew Fischbach, Myers Lab http://csbio.cs.umn.edu/, 2020
 # Code based on the work of Xiaotong Liu from Myers Lab
 
@@ -24,20 +24,20 @@ from scipy import stats
 import matplotlib.pyplot as plt
 
 
-def setUp(argv, targetFileName, refFileName, cpus, prefix, mem):
+def setUp(argv, targetFileName, refFileName, cpus, prefix, regionLength):
     print('\nChecking input')
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
         '-t', '--tarHaps', type=str, required=True, help='Target GWAS dataset in vcfn vcf bgziped, or bcf format as a singular file. This program will do the separation')
     parser.add_argument(
-        '-r', '--refHaps', type=str, required=True, help='File list containing reference datasets (in order of chromosomes 1-22, one per line) in imp5 format.')
+        '-r', '--refHaps', type=str, required=True, help='File list containing reference datasets (in order of chromosomes 1-22, one per line) in imp5 format with an idx index. Use imp5Converter from impute5 to create these files.')
     parser.add_argument(
         '-p', '--prefix', type=str, default='ouput', help='Prefix for output files.')
     parser.add_argument(
         '-c', '--cpus', type=str, default='1', help='Number of threads to use. Suggested around 12 cores with approx 100 Gb RAM.')
     parser.add_argument(
-        '-m', '--memory', type=str, default='10000000', help='length of region to impute at a time. 10 cM is default. To reduce memory usage, use less than 10000000.')
+        '-l', '--region_length', type=str, default='10000000', help='length of region to impute at a time (in bp!). 5-15 Mbp is typical and can be changed depending on available memory.')
     args = parser.parse_args()
 
     targetFileName = os.path.abspath(args.tarHaps)
@@ -50,14 +50,14 @@ def setUp(argv, targetFileName, refFileName, cpus, prefix, mem):
     cpus = args.cpus
     prefix = args.prefix
     try:
-        int(args.memory)
+        int(args.region_length)
     except:
-        print('please supply a number to -m/--memory')
+        print('please supply a number to -l/--region_length')
         sys.exit(1)
-    mem = args.memory
+    regionLength = args.region_length
     print('Successful\n')
 
-    return targetFileName, refFileName, cpus, prefix, mem
+    return targetFileName, refFileName, cpus, prefix, regionLength
 
 
 def runCommandLine(commands):
@@ -116,16 +116,21 @@ def splitVCF(file, cpus):
             for i in range(1, 23):
                 line = str(i) + " chr" + str(i) + "\n"
                 f.write(line)
+
+    extractChr = ''
+    for i in range(1, 23):
+        extractChr = extractChr + "chr" + str(i) + ","
+    extractChr = extractChr[:-1]
     
-    runCommandLine(["bcftools sort -Ou -T beforePhasing/ " + file + 
-    " | bcftools annotate --rename-chrs beforePhasing/change_chromosomes_tmp.txt -Ob -o beforePhasing/sortedTargetFile.tmp.bcf --threads " + cpus])
+    runCommandLine(["bcftools annotate " + file + " --rename-chrs beforePhasing/change_chromosomes_tmp.txt -Ou \
+        | bcftools view -Ou -t " + extractChr + " | bcftools sort -T beforePhasing/ -Ob -o beforePhasing/sortedTargetFile.tmp.bcf"])
     runCommandLine(['tabix -f beforePhasing/sortedTargetFile.tmp.bcf'])
 
-    print('separating chromosomes 1-22')
+    print('separating into chromosomes 1-22 and remove indels')
     for i in range(1, 23):
         chrm = str(i)
         runCommandLine(["bcftools view --threads " + cpus + " -Ob -o beforePhasing/chr" + chrm + ".tmp.bcf \
-            -r chr" + chrm + " beforePhasing/sortedTargetFile.tmp.bcf"])
+            -r chr" + chrm + " -V indels beforePhasing/sortedTargetFile.tmp.bcf"])
 
 def sortAndFilterShapeit4(chrm, cpus):
     # remove duplicate. File should be vcf.gz
@@ -140,14 +145,14 @@ def sortAndFilterShapeit4(chrm, cpus):
 def prePhaseShapeit4(chrm, cpu):
     print('beginning prephasing with Shapeit4')
     runCommandLine(["shapeit4 --input beforePhasing/chr" + chrm + ".prephaseReady.tmp.bcf \
-        --map map/shapeit4_maps/chr" + chrm + ".b38.gmap.gz \
+        --map map/chr" + chrm + ".b38.gmap.gz \
         --region chr" + chrm + " \
         --thread " + cpu + " \
-        --output afterPhasing/prePhasedChr" + chrm + "_needGENO.tmp.bcf --log afterPhasing/prePhasedChr" + chrm + ".tmp.log"])
+        --output afterPhasing/prePhasedChr" + chrm + ".tmp.bcf --log afterPhasing/prePhasedChr" + chrm + ".tmp.log"])
 
-def runImpute5(chrm, refFile, cpus, mem):
+def runImpute5(chrm, refFile, cpus, regionLength):
     print('\nBeginning Impute5 Imputation')
-    region = int(mem)  # 10 cM default
+    region = int(regionLength)  # 10 cM default
 
     # Find how long the chromosome is using the GRCh38 fasta file index
     commandHeader = "grep -w chr" + chrm + \
@@ -171,10 +176,10 @@ def runImpute5(chrm, refFile, cpus, mem):
         end = length if i == (int(div) - 1) else (i + 1) * region
         step = str(i + 1)
         commandLine = ["impute5 --g afterPhasing/prePhasedChr" + chrm + ".tmp.bcf \
-            --h " + refFile.strip() + " --m map/impute5_maps/chr" + chrm + ".b38.gmap.gz \
+            --h " + refFile.strip() + " --m map/chr" + chrm + ".b38.gmap.gz \
             --r chr" + chrm + ":" + str(start) + "-" + str(end) + " --o output/chr" + 
             chrm + "/impute5_" + step + ".tmp.bcf \
-            --l output/chr" + chrm + "/impute5_" + step + ".log --out-gp-field"]
+            --l output/chr" + chrm + "/impute5_" + step + ".tmp.log --out-gp-field"]
         commands.append(commandLine)
         imputeList.append("output/chr" + chrm + "/impute5_" + step + ".tmp.bcf")
     # the following code was found at https://stackoverflow.com/questions/14533458/python-threading-multiple-bash-subprocesses/14533902#14533902
@@ -182,7 +187,8 @@ def runImpute5(chrm, refFile, cpus, mem):
     pool = Pool(max_workers) # run the value of 'cpus' concurrent commands at a time
     for i, returncode in enumerate(pool.imap(partial(subprocess.call, shell=True, stdout=subprocess.DEVNULL), commands)):
         if returncode != 0:
-            print("region %d failed: %d\nView output/Chr%d/impute5_%d.log for more details" % ((i+1), returncode, int(chrm), (i+1)))
+            print("region %d failed: %d, likely there were no variants included and will still run to completeion.\
+                \nView output/Chr%d/impute5_%d.tmp.log for more details" % ((i+1), returncode, int(chrm), (i+1)))
 
     # concat vcf files together
     imputeString = ''
@@ -194,33 +200,36 @@ def runImpute5(chrm, refFile, cpus, mem):
 
 
 def setUpCompareImpute5(cpus, chrm):
-    infoFile = "output/chr" + chrm + "/impute5_all_MAF_GENO.tmp.vcf.gz"
-    # bcftools MAF calculation 25:69min. Can bring it down to 11min if I filter by MAF without using fill-tags
-    runCommandLine(["bcftools +fill-tags --no-version --threads " + cpus + " output/chr" + chrm + "/impute5_all.tmp.bcf -Ob -o output/chr" + chrm + "/impute5_all_MAF.tmp.bcf -- -t MAF"])
-    
     # find which variants are genotyped 7:7min
-    runCommandLine(["bcftools query -f '%CHROM\t%POS\t1\n' -o output/chr" + chrm + "/region_list_tmp.txt afterPhasing/prePhasedChr" + chrm + "_needGENO.tmp.bcf"])
+    runCommandLine(["bcftools query -f '%CHROM\t%POS\t1\n' -o output/chr" + chrm + "/region_list_tmp.txt afterPhasing/prePhasedChr" + chrm + ".tmp.bcf"])
     runCommandLine(["bgzip -f output/chr" + chrm + "/region_list_tmp.txt; tabix -s1 -b2 -e2 output/chr" + chrm + "/region_list_tmp.txt.gz"])
     # Create annotation file for markers that are similar b/t before and after imputation, and filter for IMP flag
-    runCommandLine(["bcftools query -T output/chr" + chrm + "/region_list_tmp.txt.gz output/chr" + chrm + "/impute5_all_MAF.tmp.bcf -f '%CHROM\t%POS\t1\n' -o output/chr" + chrm + "/annot_geno_list.tmp.txt"])
+    runCommandLine(["bcftools query -T output/chr" + chrm + "/region_list_tmp.txt.gz output/chr" + chrm + "/impute5_all.tmp.bcf -f '%CHROM\t%POS\t1\n' -o output/chr" + chrm + "/annot_geno_list.tmp.txt"])
     runCommandLine(["bgzip -f output/chr" + chrm + "/annot_geno_list.tmp.txt; tabix -s1 -b2 -e2 output/chr" + chrm + "/annot_geno_list.tmp.txt.gz"])
 
     # Change header lines
     runCommandLine(["""echo '##INFO=<ID=GENO,Number=0,Type=Flag,Description="Genotyped marker">' > output/chr""" + chrm + "/header_lines_tmp"])
     # Annotate imputed file 7:52min
-    runCommandLine(["bcftools annotate --no-version -a output/chr" + chrm + "/annot_geno_list.tmp.txt.gz -h output/chr" + chrm + "/header_lines_tmp -c CHROM,POS,INFO/GENO output/chr" + chrm + "/impute5_all_MAF.tmp.bcf \
-        -Ob -o output/chr" + chrm + "/impute5_all_MAF_GENO.tmp.bcf --threads " + cpus])
-    # prepare INFO like file (just remove the genotypes)1:10min
-    runCommandLine(["bcftools view --no-version -G output/chr" + chrm + "/impute5_all_MAF_GENO.tmp.bcf -Oz -o " + infoFile + " --threads " + cpus])
+    runCommandLine(["bcftools annotate --no-version -a output/chr" + chrm + "/annot_geno_list.tmp.txt.gz -h output/chr" + chrm + "/header_lines_tmp -c CHROM,POS,INFO/GENO output/chr" + chrm + "/impute5_all.tmp.bcf \
+        -Ob -o output/chr" + chrm + "/impute5_all_GENO.tmp.bcf --threads " + cpus])
     
-    print("\topening info file as a data frame...")
-    df = allel.vcf_to_dataframe(infoFile, fields=['variants/*'])
-    return df, infoFile
+    # create separate files of MAFs < 0.05%, 0.05-5%, and > 5%, then make vcf.gz files of just variant information without GT
+    infoFile =[]
+    for i in (["'MAF <= 0.0005'", "0-0.05"], ["'MAF > 0.0005 && MAF <= 0.05'", "0.05-5"], ["'MAF > 0.05'", "5-50"]):
+        runCommandLine(["bcftools view --no-version output/chr" + chrm + "/impute5_all_GENO.tmp.bcf -i " + i[0] +  
+            " --threads " + cpus + " -Ob -o output/chr" + chrm + "/impute5_all_GENO_" + i[1] + ".tmp.bcf"])
+
+        runCommandLine(["bcftools view --no-version -G output/chr" + chrm + "/impute5_all_GENO_" + i[1] + ".tmp.bcf \
+            -Oz -o output/chr" + chrm + "/impute5_all_GENO_" + i[1] + ".tmp.vcf.gz --threads " + cpus])
+        
+        infoFile.append("output/chr" + chrm + "/impute5_all_GENO_" + i[1] + ".tmp.vcf.gz")
+    
+    return infoFile
 
 def printInfoImpute(dfInput, typeVar, csvList, maf, chrm):
     csvList.append([chrm, typeVar , maf, round(dfInput['INFO'].mean(), 4)])
 
-def createGraphs(df, infoFile, chrm):
+def createGraphs(df, prefix, outputDir, chrm):
     print('new plot')
     binNum = 30
     all_maf_0_1 = df.loc[(df['MAF'] <= 0.01)]
@@ -249,44 +258,35 @@ def createGraphs(df, infoFile, chrm):
     plt.legend()
     plt.xscale('log')
     plt.tight_layout(pad=1.0)
-    plt.savefig(infoFile[:-7] + '_infoScoreByMaf.jpeg')
+    plt.savefig(outputDir + prefix + '_chr' + chrm + '_infoScoreByMaf.jpeg')
 
-def compareImpute5(cpus, chrm):
+def compareImpute5(cpus, prefix, outputDir, chrm):
     print("Starting accuracy analysis")
-    df, infoFile = setUpCompareImpute5(cpus, chrm)
-    df.rename(columns={"MAF_1": "MAF"}, inplace=True)
-    print("\tbeginning accuracy calculations...")
     csvList = []
+    infoFile = setUpCompareImpute5(cpus, chrm)
 
-    mafLess_0_05_GENO = df.loc[(df['MAF'] <= 0.0005) & (df['IMP'] == False) & (df['GENO'] == True) ]
-    maf_0_05_to_5_GENO = df.loc[(df['MAF'] > 0.0005) & (df['MAF'] <= 0.05) & (df['IMP'] == False) & (df['GENO'] == True) ]
-    maf_5_to_50_GENO = df.loc[(df['MAF'] > 0.05) & (df['MAF'] <= 0.5) & (df['IMP'] == False) & (df['GENO'] == True) ]
+    df1 = allel.vcf_to_dataframe(infoFile[0], fields=['variants/*'])
+    df2 = allel.vcf_to_dataframe(infoFile[1], fields=['variants/*'])
+    df3 = allel.vcf_to_dataframe(infoFile[2], fields=['variants/*'])
+
+    mafLess_0_05_GENO = df1.loc[(df1['IMP'] == False) & (df1['GENO'] == True) ]
+    maf_0_05_to_5_GENO = df2.loc[(df2['IMP'] == False) & (df2['GENO'] == True) ]
+    maf_5_to_50_GENO = df3.loc[(df3['IMP'] == False) & (df3['GENO'] == True) ]
     printInfoImpute(mafLess_0_05_GENO, 'genotyped', csvList, '0-0.05%', chrm)
     printInfoImpute(maf_0_05_to_5_GENO, 'genotyped', csvList, '0.05-5%', chrm)
     printInfoImpute(maf_5_to_50_GENO, 'genotyped', csvList, '5-50%', chrm)
 
-    mafLess_0_05_IMP = df.loc[(df['MAF'] <= 0.0005) & (df['IMP'] == True) & (df['GENO'] == False) ]
-    maf_0_05_to_5_IMP = df.loc[(df['MAF'] > 0.0005) & (df['MAF'] <= 0.05) & (df['IMP'] == True) & (df['GENO'] == False) ]
-    maf_5_to_50_IMP = df.loc[(df['MAF'] > 0.05) & (df['MAF'] <= 0.5) & (df['IMP'] == True) & (df['GENO'] == False) ]
+    mafLess_0_05_IMP = df1.loc[(df1['IMP'] == True) & (df1['GENO'] == False) ]
+    maf_0_05_to_5_IMP = df2.loc[(df2['IMP'] == True) & (df2['GENO'] == False) ]
+    maf_5_to_50_IMP = df3.loc[(df3['IMP'] == True) & (df3['GENO'] == False) ]
     printInfoImpute(mafLess_0_05_IMP, 'imputed', csvList, '0-0.05%', chrm)
     printInfoImpute(maf_0_05_to_5_IMP, 'imputed', csvList, '0.05-5%', chrm)
     printInfoImpute(maf_5_to_50_IMP, 'imputed', csvList, '5-50%', chrm)
 
-    mafLess_0_05_all = df.loc[(df['MAF'] <= 0.0005) ]
-    maf_0_05_to_5_all = df.loc[(df['MAF'] > 0.0005) & (df['MAF'] <= 0.05) ]
-    maf_5_to_50_all = df.loc[(df['MAF'] > 0.05) & (df['MAF'] <= 0.5) ]
-    printInfoImpute(mafLess_0_05_all, 'all', csvList, '0-0.05%', chrm)
-    printInfoImpute(maf_0_05_to_5_all, 'all', csvList, '0.05-5%', chrm)
-    printInfoImpute(maf_5_to_50_all, 'all', csvList, '5-50%', chrm)
+    printInfoImpute(df1, 'all', csvList, '0-0.05%', chrm)
+    printInfoImpute(df2, 'all', csvList, '0.05-5%', chrm)
+    printInfoImpute(df3, 'all', csvList, '5-50%', chrm)
 
-    print("Number of IMP flagged markers {}".format(len(df.loc[(df['IMP'] == True) & (df['GENO'] == False)])))
-    print("Number of GENO flagged markers {}".format(len(df.loc[(df['IMP'] == False) & (df['GENO'] == True)])))
-    print("Number of IMP and GENO flagged markers {}".format(len(df.loc[(df['IMP'] == True) & (df['GENO'] == True)])))
-    print("Number of no IMP and no GENO flagged markers {}".format(len(df.loc[(df['IMP'] == False) & (df['GENO'] == False)])))
-
-    createGraphs(df, infoFile, chrm)
-
-    print("Successful")
     return csvList
 
 def writeCSV_Impute5(analysisList, chrm):
@@ -303,10 +303,14 @@ def cleanUp(prefix, cpus, outputDir):
     print("concatenating all chromosomes...")
     chrList = ''
     for i in range(1, 23):
-        chrList = chrList + "output/chr" + str(i) + "/impute5_all_MAF_GENO.tmp.bcf "
+        if os.path.isfile("output/chr" + str(i) + "/impute5_all_GENO.tmp.bcf"):
+            chrList = chrList + "output/chr" + str(i) + "/impute5_all_GENO.tmp.bcf "
     runCommandLine(["bcftools concat --no-version -Ob --threads " + cpus + " -o " + outputDir + '/' + prefix + ".bcf " + chrList])
+
     print("removing temporary files and directories...")
-        
+    runCommandLine(["rm beforePhasing/*tmp*"])
+    runCommandLine(["rm afterPhasing/*tmp*"])
+    runCommandLine(["rm output/chr*/*tmp*"])
 
 def main():
     # Set up argument inputs
@@ -314,9 +318,9 @@ def main():
     prefix = ''
     refFileName = ''
     cpus = ''
-    mem =''
+    regionLength =''
     
-    targetFileName, refFileName, cpus, prefix, mem = setUp(sys.argv[1:], targetFileName, refFileName, cpus, prefix, mem)
+    targetFileName, refFileName, cpus, prefix, regionLength = setUp(sys.argv[1:], targetFileName, refFileName, cpus, prefix, regionLength)
     
     with open(refFileName) as f:
         refFileList = f.readlines()
@@ -336,10 +340,10 @@ def main():
         prePhaseShapeit4(chromosome, cpus)
 
         refFile = os.path.dirname(refFileName) + "/" + refFileList[i-1]
-        runImpute5(chromosome, refFile, cpus, mem)
+        runImpute5(chromosome, refFile, cpus, regionLength)
         print("Successful chromosome " + chromosome + " imputation\n")
 
-        analysisList = compareImpute5(cpus, chromosome)
+        analysisList = compareImpute5(cpus, prefix, outputDir, chromosome)
         writeCSV_Impute5(analysisList, chromosome)
         print('Successful accuracy analysis\n')
     
@@ -351,5 +355,4 @@ if __name__ == '__main__':
     main()
 
 
-# ../../ImputationAccuracy/Impute5Pipeline.py -t PD_CIDR_phs000126.GRCh38.vcf.gz -r ../../ImputationAccuracy/reference_1000G/impute5Ref/refFileList -c 12 -p outputTesting
-
+# nohup ../../ImputationAccuracy/Impute5Pipeline.py -t PD_CIDR_phs000126.GRCh38.vcf.gz -r ../../ImputationAccuracy/reference_1000G/impute5Ref/refFileList -c 12 -p outputTesting > output.log &
